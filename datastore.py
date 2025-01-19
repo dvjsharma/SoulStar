102b590112d4f1b48747de3caabefcb3f434b86c
Divij Sharma <divijs75@gmail.com>
2025-01-19 07:02:21 +0530

    feat: Add dynamic suggestions

---
+++ datastore.py
@@ -0,0 +1,89 @@
+suggestions_store = {
+    "sug1": {
+        "text": "Suggestion 1",
+        "nested_suggestions": {
+            "sug1_1": {
+                "text": "Sub-suggestion 1.1",
+                "nested_suggestions": {
+                    "sug1_1_1": "Deep suggestion 1.1.1",
+                    "sug1_1_2": "Deep suggestion 1.1.2",
+                    "sug1_1_3": "Deep suggestion 1.1.3",
+                }
+            },
+            "sug1_2": {
+                "text": "Sub-suggestion 1.2",
+                "nested_suggestions": {
+                    "sug1_2_1": "Deep suggestion 1.2.1",
+                    "sug1_2_2": "Deep suggestion 1.2.2",
+                    "sug1_2_3": "Deep suggestion 1.2.3",
+                }
+            },
+            "sug1_3": {
+                "text": "Sub-suggestion 1.3",
+                "nested_suggestions": {
+                    "sug1_3_1": "Deep suggestion 1.3.1",
+                    "sug1_3_2": "Deep suggestion 1.3.2",
+                    "sug1_3_3": "Deep suggestion 1.3.3",
+                }
+            }
+        }
+    },
+    "sug2": {
+        "text": "Suggestion 2",
+        "nested_suggestions": {
+            "sug2_1": {
+                "text": "Sub-suggestion 2.1",
+                "nested_suggestions": {
+                    "sug2_1_1": "Deep suggestion 2.1.1",
+                    "sug2_1_2": "Deep suggestion 2.1.2",
+                    "sug2_1_3": "Deep suggestion 2.1.3",
+                }
+            },
+            "sug2_2": {
+                "text": "Sub-suggestion 2.2",
+                "nested_suggestions": {
+                    "sug2_2_1": "Deep suggestion 2.2.1",
+                    "sug2_2_2": "Deep suggestion 2.2.2",
+                    "sug2_2_3": "Deep suggestion 2.2.3",
+                }
+            },
+            "sug2_3": {
+                "text": "Sub-suggestion 2.3",
+                "nested_suggestions": {
+                    "sug2_3_1": "Deep suggestion 2.3.1",
+                    "sug2_3_2": "Deep suggestion 2.3.2",
+                    "sug2_3_3": "Deep suggestion 2.3.3",
+                }
+            }
+        }
+    },
+    "sug3": {
+        "text": "Suggestion 3",
+        "nested_suggestions": {
+            "sug3_1": {
+                "text": "Sub-suggestion 3.1",
+                "nested_suggestions": {
+                    "sug3_1_1": "Deep suggestion 3.1.1",
+                    "sug3_1_2": "Deep suggestion 3.1.2",
+                    "sug3_1_3": "Deep suggestion 3.1.3",
+                }
+            },
+            "sug3_2": {
+                "text": "Sub-suggestion 3.2",
+                "nested_suggestions": {
+                    "sug3_2_1": "Deep suggestion 3.2.1",
+                    "sug3_2_2": "Deep suggestion 3.2.2",
+                    "sug3_2_3": "Deep suggestion 3.2.3",
+                }
+            },
+            "sug3_3": {
+                "text": "Sub-suggestion 3.3",
+                "nested_suggestions": {
+                    "sug3_3_1": "Deep suggestion 3.3.1",
+                    "sug3_3_2": "Deep suggestion 3.3.2",
+                    "sug3_3_3": "Deep suggestion 3.3.3",
+                }
+            }
+        }
+    }
+}
